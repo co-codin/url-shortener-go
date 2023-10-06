9b1009bc7109ff58dfa47e396003627e63c57013
elijah <cuiyeqing960904@gmail.com>
2023-10-06 07:31:14 -0400

    building storage

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/exp/slog"
+	"os"
+	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting url-shortener",
+		slog.String("env", cfg.Env),
+		slog.String("version", "123"),
+	)
+	log.Debug("debug messages are enabled")
 }
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	// case envLocal:
+	// 	log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+// func setupPrettySlog() *slog.Logger {
+// 	opts := slogpretty.PrettyHandlerOptions{
+// 		SlogOpts: &slog.HandlerOptions{
+// 			Level: slog.LevelDebug,
+// 		},
+// 	}
+
+// 	handler := opts.NewPrettyHandler(os.Stdout)
+
+// 	return slog.New(handler)
+// }
